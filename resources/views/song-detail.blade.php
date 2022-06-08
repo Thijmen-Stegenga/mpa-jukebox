878a39aaca5850c4daddb0ba24b4d30f273dee79
Thijmen Stegenga <t.j.stegenga@gmail.com>
2022-06-08 09:53:20 +0200

    Fixed the song detail page

--- resources/views/song-detail.blade.php
+++ resources/views/song-detail.blade.php
@@ -12,7 +12,7 @@
        <div class="col-4 mt-5">
         <div class="card">
           <img class="mx-auto mt-4 border border-" src={{$songs->img}}>
-          <h5><th>{{$songs->name}}<th></h5>
+          <h5><th>{{$songs->name}}t<th></h5>
           <h5><th>{{$songs->artist}}</th></h5>
           <h5><th>{{$songs->duration}}</th></h5>
           <h5><th>{{$songs->genre}}</th></h5>
